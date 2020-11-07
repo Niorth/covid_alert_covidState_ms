a3aa8b56aeb8ce61351947784d662c34ae8f93ab
paolaANDREU <paola.andreu@etu.umontpellier.fr>
2020-11-07 14:20:56 +0100

    init ms covidstate

---
+++ src/main/java/fr/projetiwa/covid_alert_covidState_ms/CovidAlertCovidStateMsApplication.java
@@ -0,0 +1,13 @@
+package fr.projetiwa.covid_alert_covidState_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidAlertCovidStateMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidAlertCovidStateMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/projetiwa/covid_alert_covidState_ms/CovidAlertCovidStateMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.projetiwa.covid_alert_covidState_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidAlertCovidStateMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
