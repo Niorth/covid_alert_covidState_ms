8774ff58568118900ec8f062138a6290571aaec4
GuillaumeGT <guilherme.t14@gmail.com>
2020-11-13 14:20:18 +0100

    Delete getLastPersonStateByPersonId

--- src/main/java/fr/projetiwa/covid_alert_covidState_ms/repositories/PersonStateRepository.java
+++ src/main/java/fr/projetiwa/covid_alert_covidState_ms/repositories/PersonStateRepository.java
@@ -12,7 +12,4 @@ public interface PersonStateRepository extends JpaRepository<PersonState,Long> {
     @Query("select p.covidState.stateLabel from personState p where p.personId = :personId and p.date = (select max(p2.date) from personState p2 where p2.personId = :personId)")
     String getLastCovidStateLabelByPersonId(@Param("personId")Long personId);
 
-    @Query("select p.personStateId, p.personId, p.date, p.covidState from personState p where p.personId = :personId and p.date = (select max(p2.date) from personState p2 where p2.personId = :personId)")
-    PersonState getLastPersonStateByPersonId(@Param("personId") Long personId);
-
 }
