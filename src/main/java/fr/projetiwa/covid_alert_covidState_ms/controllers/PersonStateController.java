d7586dca0e0fac588a8e4b982423ad7fe4fb2c11
GuillaumeGT <guilherme.t14@gmail.com>
2020-11-20 22:20:42 +0100

    Use token in POST personState/update, send to ms_susposition when positive

--- src/main/java/fr/projetiwa/covid_alert_covidState_ms/controllers/PersonStateController.java
+++ src/main/java/fr/projetiwa/covid_alert_covidState_ms/controllers/PersonStateController.java
@@ -10,11 +10,14 @@ import org.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.UnsupportedEncodingException;
 import java.security.Principal;
 import java.time.Instant;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -52,17 +55,48 @@ public class PersonStateController {
     }
 
     @PostMapping("/update")
-    public Boolean changePersonState(Principal principal, @RequestBody String req){
-        JSONObject json = new JSONObject(req);
-        Long stateId = json.getLong("stateId");
-        String personId = principal.getName();
-        CovidState selectedCovidState = covidStateRepository.getCovidStateByStateId(stateId);
-        PersonState newPersonState = new PersonState();
-        newPersonState.setPersonId(personId);
-        newPersonState.setDate(Date.from(Instant.now()));
-        newPersonState.setCovidState(selectedCovidState);
-        personStateRepository.saveAndFlush(newPersonState);
-        return true;
+    public String changePersonState(@RequestHeader (name="Authorization") String token, @RequestBody String req){
+        String payload = token.split("\\.")[1];
+
+        try {
+            String str = new String(Base64.decodeBase64(payload),"UTF-8");
+            JSONObject jsonObject = new JSONObject(str);
+            String personId = jsonObject.getString("sub");
+            JSONObject json = new JSONObject(req);
+            Long stateId = json.getLong("stateId");
+            CovidState selectedCovidState = covidStateRepository.getCovidStateByStateId(stateId);
+            PersonState newPersonState = new PersonState();
+            newPersonState.setPersonId(personId);
+            newPersonState.setDate(Date.from(Instant.now()));
+            newPersonState.setCovidState(selectedCovidState);
+            personStateRepository.saveAndFlush(newPersonState);
+
+
+            //Check if new state is positive
+            if(selectedCovidState.isPositive()){
+                String urlPositions = "http://localhost:3003/positions/setSuspicious";
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+                headers.add(HttpHeaders.AUTHORIZATION, token);
+                HttpEntity request = new HttpEntity(headers);
+                RestTemplate restTemplate = new RestTemplate();
+                String serviceUrl = urlPositions;
+                //System.out.println(headers);
+                ResponseEntity<String> response = restTemplate.exchange( serviceUrl, HttpMethod.POST, request, String.class, 1 );
+            }
+
+
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+        return "{\"success\":1}";
     }
 
 }
